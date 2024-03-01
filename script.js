9af910e539b582ed79d4e25d023ae655e37e16d4
handomekal <handomekal@gmail.com>
2024-02-29 22:59:34 -0500

    defined variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors')
+const resultDisplay = document.getElementById('result');
