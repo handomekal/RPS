122694638c6f99e4e1099d81cb2aa4a2c3d29e04
handomekal <handomekal@gmail.com>
2024-02-29 23:19:35 -0500

    error fixed

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ scissorsButton.addEventListener('click', () => playRound('scissors'));
 function playRound(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    
-    if (playerChoice === computer) {
+
+    if (playerChoice === computerChoice) {
         resultDisplay.textContent = 'It\'s a draw!';
     } else if (
         (playerChoice === 'rock' && computerChoice === 'scissors') ||
         (playerChoice === 'paper' && computerChoice === 'rock') ||
-        (playerChoice === 'scissors' && computerChoice === 'paper') 
-    )   {
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
         resultDisplay.textContent = 'You win!';
     } else {
         resultDisplay.textContent = 'Computer wins!'
     }
+    console.log(resultDisplay)
 }
